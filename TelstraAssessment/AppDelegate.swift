26193086ad20158c221355bef28070e870bfdd2f
Deepak Pillai <deepak@deepaks-macbook-pro.local>
2018-10-23 15:11:19 +0530

    Removed storyboard from project

--- TelstraAssessment/AppDelegate.swift
+++ TelstraAssessment/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        //RootVC
+        window = UIWindow(frame: UIScreen.main.bounds)
+        var navController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navController
+        window?.backgroundColor = UIColor.white
+        window?.makeKeyAndVisible()
+        
         return true
     }
 
