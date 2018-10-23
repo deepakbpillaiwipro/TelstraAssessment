ea55d36fa21268532403e13a055b0fa9e462d984
Deepak Pillai <deepak@Deepaks-MacBook-Pro.local>
2018-10-23 14:49:50 +0530

    First commit

---
+++ TelstraAssessment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TelstraAssessment
+//
+//  Created by Deepak Pillai on 23/10/18.
+//  Copyright © 2018 Wipro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
